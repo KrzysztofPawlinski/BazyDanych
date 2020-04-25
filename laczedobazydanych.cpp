87c58d798bb10a31f0398cd5e1388c2e7e4eaab6
Krzysztof Pawliński <krzysztof.pawlinski1@gmail.com>
2020-04-25 20:46:31 +0200

    dodano obsluge bazy danych

--- laczedobazydanych.cpp
+++ laczedobazydanych.cpp
@@ -1,6 +1,38 @@
 #include "laczedobazydanych.h"
+#include <iostream>
 
-laczeDoBazyDanych::laczeDoBazyDanych()
+laczeDoBazyDanych::laczeDoBazyDanych(QObject *parent) : QObject(parent) {}
+
+laczeDoBazyDanych::laczeDoBazyDanych(std::string host, std::string name, std::string user, std::string passwd, int port, QObject *parent) :
+    QObject(parent)
 {
+    if(connect(host, name, user, passwd, port))
+        std::cout<<"Otwarto baze"<<std::endl;
+    else{
+        std::cout<<"Nie udalo sie otworzyc bazy"<<std::endl;
+        check = false;
+    }
+}
 
+bool laczeDoBazyDanych::getcheck() {
+    return check;
+}
+
+bool laczeDoBazyDanych::connect(std::string host, std::string name, std::string user, std::string passwd, int port){
+    db.setConnectOptions();
+    db.setPort(port);
+    db.setHostName(QString::fromStdString(host));
+    db.setDatabaseName(QString::fromStdString(name));
+    db.setUserName(QString::fromStdString(user));
+    db.setPassword(QString::fromStdString(passwd));
+    return this->db.open();
+}
+
+void laczeDoBazyDanych::init(std::string host, std::string name, std::string user, std::string passwd, int port){
+    if(connect(host, name, user, passwd, port))
+        std::cout<<"Otwarto baze"<<std::endl;
+    else{
+        std::cout<<"Nie udalo sie otworzyc bazy"<<std::endl;
+        check = false;
+    }
 }
--- laczedobazydanych.h
+++ laczedobazydanych.h
@@ -1,11 +1,31 @@
 #ifndef LACZEDOBAZYDANYCH_H
 #define LACZEDOBAZYDANYCH_H
 
+#include <QSqlDatabase>
+#include <QObject>
+#include "klient.h"
+#include "kierowca.h"
+#include "samochod.h"
 
-class laczeDoBazyDanych
+class laczeDoBazyDanych : QObject
 {
+    QSqlDatabase db = QSqlDatabase::addDatabase("QOCI");
+    bool check = true;
 public:
-    laczeDoBazyDanych();
+    laczeDoBazyDanych(QObject *parent=0);
+    laczeDoBazyDanych(std::string host, std::string name, std::string user, std::string passwd, int port, QObject *parent=0);
+    bool connect(std::string host, std::string name, std::string user, std::string passwd, int port);
+    QSqlDatabase getdb(){
+        return db;
+    }
+
+    void init(std::string host, std::string name, std::string user, std::string passwd, int port);
+    bool getcheck();
+
+
+signals:
+    void UtraconoPolaczenie();
+
 };
 
 #endif // LACZEDOBAZYDANYCH_H
